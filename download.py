d476529f502f7c1d05c1f88be5480c593586293d
NOOBDY <htommyf12@gmail.com>
2019-11-19 15:41:02 +0800

    changed something

--- download.py
+++ download.py
@@ -1,14 +1,15 @@
 from google_images_download import google_images_download
 
-response = google_images_download.googleimagesdownload()
-
 i = input("Search something: ")
 
-args = {
-    "keywords": "{}".format(i),
-    "limit": 10,
-    "print_urls": True
-}
+def download(i, limit):
+    response = google_images_download.googleimagesdownload()
 
-paths = response.download(args)
-print(paths)
+    args = {
+        "keywords": "{}".format(i),
+        "limit": 10,
+        "print_urls": True
+    }
+
+    paths = response.download(args)
+    print(paths)
