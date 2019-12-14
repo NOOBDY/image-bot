b49dd4c42ea7a60471ee9d6491ca5c18055420a0
NOOBDY <htommyf12@gmail.com>
2019-12-14 10:50:52 +0800

    added porn command

---
+++ APIs/pornhub.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+
+def pornhub(keywords, limit=10):
+
+    keyword = "+".join(keywords)
+    BASE_URL = "https://pornhub.com"
+    SEARCH_URL = f"{BASE_URL}/video/search?search={keyword}"
+    
+    response = requests.get(SEARCH_URL)
+    html = response.content
+    soup = BeautifulSoup(html, "html.parser")
+    as_ = soup.find_all("a", class_="linkVideoThumb")
+
+    titles = []
+    urls = []
+    thumbs = []
+
+    for a in as_[4:]:
+        titles.append(a['title'])
+        urls.append(f"{BASE_URL}{a['href']}")
+        thumbs.append(a.img['data-src'])
+    
+    r_keyword = keyword.replace("+", " ")
+    
+    if len(urls) > 10:
+        print(">10")
+        return r_keyword, titles[:10], urls[:10], thumbs[:10]
+    else:
+        return r_keyword, titles, urls, thumbs
+
+if __name__ == "__main__":
+    from random import randint
+    keyword, titles, urls, thumbs = pornhub(["ableton"]);
+    i = randint(0, len(titles) - 1)
+    print(i)
+    print(len(titles))
+    print(titles[i])
+    print(urls[i])
+    print(thumbs[i])
+    print()
---
+++ cogs/weird_functions.py
@@ -0,0 +1,40 @@
+import os
+import discord
+from discord.ext import commands
+import sys
+from random import randint
+from APIs.image import image
+from APIs.pornhub import pornhub
+
+class Weird(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def image(self, ctx, *args):
+        keyword, urls = image(args)
+        if urls is None:
+            ctx.send("No image found or some error occured. Please try again.")
+        else:
+            url = urls[randint(0, len(urls) - 1)]
+            embed = discord.Embed(
+                title=f"Search results for: {keyword}",
+                description=url,
+                color=0xff8000)
+            embed.set_image(url=url)
+            await ctx.send(embed=embed)
+
+    @commands.command()
+    async def porn(self, ctx, *args):
+        keyword, titles, urls, thumbs = pornhub(args)
+        i = randint(0, len(urls) - 1)
+        title = titles[i]
+        url = urls[i]
+        thumb = thumbs[i]
+        embed=discord.Embed(title=f"Search results for: {keyword}")
+        embed.set_thumbnail(url=thumb)
+        embed.add_field(name=title, value=url, inline=True)
+        await ctx.send(embed=embed)
+
+def setup(client):
+    client.add_cog(Weird(client))
